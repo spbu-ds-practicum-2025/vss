a91a0ec2ca0702f7c427b9dfce9a94a01873c12b
Alexander Kuka <lxndrkk6002@gmail.com>
2025-11-21 21:51:39 +0300

    first version of api_gateaway

--- services/worker/main.py
+++ services/worker/main.py
@@ -1,57 +1,71 @@
 from worker import WordCountMapper, MapExecutor, WordCountShuffler, ShuffleExecutor, WordCountReducer, ReduceExcecutor, DataManager
 from loaders import txtDataSource, jsonDataSink, txtDataSink, jsonDataSource
+from pathlib import Path
 
-import shutil
+import shutil, time
 import os
 
 
+def wait_for_file(file_path: str, check_interval: float = 5.0):
+    path = Path(file_path)
+
+    
+    while not path.exists():
+        time.sleep(check_interval)
+    
+    return True
+
 if __name__ == "__main__":
+    while True:
+        # cleanup previous run directories
+        dirs = [r'worker\spill_files',
+                r'worker\shuffle_files',
+                r'worker\reduce_output']
+        
+        for d in dirs:
+            if os.path.exists(d):
+                shutil.rmtree(d)
 
-    # cleanup previous run directories
-    dirs = [r'C:\ovr_pr\worker\spill_files',
-            r'C:\ovr_pr\worker\shuffle_files',
-            r'C:\ovr_pr\worker\reduce_output']
-    
-    for d in dirs:
-        if os.path.exists(d):
-            shutil.rmtree(d)
+        file_path = os.path.join(os.getcwd(),'process.txt')
+        wait_for_file(file_path)
 
-    # mapping phase
-    mapper = WordCountMapper()
-    data_spill_saver = jsonDataSink(r"C:\ovr_pr\worker\spill_files", mode="jsonl")
-    data_sorce = txtDataSource()
-    
-    map_executor = MapExecutor(mapper, data_spill_saver, data_sorce, threshold=5_000)
-    map_executor.process(filepath=r"C:\ovr_pr\large_test_words.txt")
+        # mapping phase
+        mapper = WordCountMapper()
+        data_spill_saver = jsonDataSink(r"worker\spill_files", mode="jsonl")
+        data_sorce = txtDataSource()
+        
+        map_executor = MapExecutor(mapper, data_spill_saver, data_sorce, threshold=5_000)
+        map_executor.process(file_path)
 
-    print("Mapping phase completed. Starting shuffling phase...")
+        print("Mapping phase completed. Starting shuffling phase...")
 
-    # shuffling phase
+        # shuffling phase
 
-    shuffler = WordCountShuffler(num_parts=4, flush_threshold=2_000)
-    shuffle_executor = ShuffleExecutor(shuffler, 
-                                       source=jsonDataSource(),
-                                        sink=jsonDataSink(r"C:\ovr_pr\worker\shuffle_files", mode="jsonl"))
-    shuffle_executor.process(r"C:\ovr_pr\worker\spill_files")
-    print("Shuffling phase completed.")
+        shuffler = WordCountShuffler(num_parts=4, flush_threshold=2_000)
+        shuffle_executor = ShuffleExecutor(shuffler, 
+                                        source=jsonDataSource(),
+                                            sink=jsonDataSink(r"worker\shuffle_files", mode="jsonl"))
+        shuffle_executor.process(r"worker\spill_files")
+        print("Shuffling phase completed.")
 
-    # reducing phase
-    reducer = WordCountReducer()
-    reduce_executor = ReduceExcecutor(reducer,
-                                      sink=jsonDataSink(r"C:\ovr_pr\worker\reduce_output", mode="jsonl"),
-                                      source=jsonDataSource())
-    for part_num in range(4):
-        reduce_executor.process(part_dir=r"C:\ovr_pr\worker\shuffle_files", part_num=part_num)
+        # reducing phase
+        reducer = WordCountReducer()
+        reduce_executor = ReduceExcecutor(reducer,
+                                        sink=jsonDataSink(r"worker\reduce_output", mode="jsonl"),
+                                        source=jsonDataSource())
+        for part_num in range(4):
+            reduce_executor.process(part_dir=r"worker\shuffle_files", part_num=part_num)
 
-    print("Reducing phase completed.")
+        print("Reducing phase completed.")
 
-    # data management phase
+        # data management phase
 
-    dirs = [r'C:\ovr_pr\worker\spill_files',
-        r'C:\ovr_pr\worker\shuffle_files']
-    
-    for d in dirs:
-        if os.path.exists(d):
-            shutil.rmtree(d)
-    
-    DataManager.manage_reduce_data(source=jsonDataSource(), sink=txtDataSink(r"C:\ovr_pr\worker\final_output"), dirpath=r"C:\ovr_pr\worker\reduce_output")
+        dirs = [r'worker\spill_files',
+            r'worker\shuffle_files']
+        
+        for d in dirs:
+            if os.path.exists(d):
+                shutil.rmtree(d)
+        
+        DataManager.manage_reduce_data(source=jsonDataSource(), sink=txtDataSink(os.getcwd()), dirpath=r"worker\reduce_output")
+        os.remove('process.txt')
