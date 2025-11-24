92e100dff1300a0cec03cdcd1e91ec282c7dbed7
Stepan <klimenkostepan30@gmail.com>
2025-11-25 01:45:40 +0300

    Added reduce task sending. Refactored code with some consts

--- services/planner/main.py
+++ services/planner/main.py
@@ -5,15 +5,17 @@ import time
 
 QUEUE_NAME = 'tasks'
 
+RABBIT_PASS = 'password'
+RABBIT_LOGIN = 'admin'
+RABBIT_HOST = 'localhost'
+RABBIT_PORT = 5672
 
-def send_task(task_type: str, address: str):
-    credentials = pika.PlainCredentials('admin', 'password')
-    params = pika.ConnectionParameters(host='localhost', port=5672, virtual_host='/', credentials=credentials)
-    conn = pika.BlockingConnection(params)
-    ch = conn.channel()
+# undone: implement file splitting
+def split_txt_file(input_file: str, lines_per_file: int = 1_000_000) -> list:
+    ...
 
-    # durable=True — очередь переживёт перезапуск брокера
-    ch.queue_declare(queue=QUEUE_NAME, durable=True)
+
+def send_map_task(ch, task_type: str, address: str):
 
     task = {
         "task_id": str(uuid.uuid4()),
@@ -28,10 +30,39 @@ def send_task(task_type: str, address: str):
     ch.basic_publish(exchange='', routing_key=QUEUE_NAME, body=body, properties=props)
 
     print(f"[Planner] sent task {task['task_id']} type={task_type}")
+
+
+def send_reduce_task(ch, task_type: str, address: str, part_id: int):
+
+    task = {
+        "task_id": str(uuid.uuid4()),
+        "type": task_type,
+        "address": address,
+        "part_id": part_id,
+        "created_at": time.time()
+    }
+    body = json.dumps(task)
+
+    # delivery_mode=2 — сделать сообщение persistent
+    props = pika.BasicProperties(delivery_mode=2, content_type='application/json')
+    ch.basic_publish(exchange='', routing_key=QUEUE_NAME, body=body, properties=props)
+
+    print(f"[Planner] sent task {task['task_id']} type={task_type} part_id={part_id}")
+
+
+def main():
+    credentials = pika.PlainCredentials(RABBIT_LOGIN, RABBIT_PASS)
+    params = pika.ConnectionParameters(host=RABBIT_HOST, port=RABBIT_PORT, virtual_host='/', credentials=credentials)
+    conn = pika.BlockingConnection(params)
+    ch = conn.channel()
+    ch.queue_declare(queue=QUEUE_NAME, durable=True)
+
+    # send_map_task(ch, "map", r"C:\ovr_pr\large_test_words.txt")
+    send_reduce_task(ch, "reduce", r"C:\ovr_pr\vss\services\worker\shuffle_files", part_id=0)
+
     conn.close()
 
 
 if __name__ == "__main__":
-    # Примеры отправки
-    send_task("map", r"C:\ovr_pr\large_test_words.txt")
+    main()
 
--- services/worker/main.py
+++ services/worker/main.py
@@ -10,20 +10,30 @@ import uuid
 QUEUE_NAME = 'tasks'
 WORKER_ID = "1"
 
+RABBIT_PASS = 'password'
+RABBIT_LOGIN = 'admin'
+RABBIT_HOST = 'localhost'
+RABBIT_PORT = 5672
 
-def process_task(file_address: str):
-    # cleanup previous run directories
-    dirs = [r'C:\ovr_pr\vss\services\worker\spill_files',
-            r'C:\ovr_pr\vss\services\worker\shuffle_files',
-            r'C:\ovr_pr\vss\services\worker\reduce_output']
-    
+SPILL_FILES_DIR = r"C:\ovr_pr\vss\services\worker\spill_files"
+SHUFFLE_FILES_DIR = r"C:\ovr_pr\vss\services\worker\shuffle_files"
+REDUCE_OUTPUT_DIR = r"C:\ovr_pr\vss\services\worker\reduce_output"
+
+DIRS = [SPILL_FILES_DIR, SHUFFLE_FILES_DIR, REDUCE_OUTPUT_DIR]
+
+
+def cleanup_previous_run_dirs(dirs: list):
     for d in dirs:
         if os.path.exists(d):
             shutil.rmtree(d)
 
+
+def process_map_task(file_address: str):
+    cleanup_previous_run_dirs(DIRS)
+
     # mapping phase
     mapper = WordCountMapper()
-    data_spill_saver = jsonDataSink(r"C:\ovr_pr\vss\services\worker\spill_files", mode="jsonl")
+    data_spill_saver = jsonDataSink(SPILL_FILES_DIR, mode="jsonl")
     data_sorce = txtDataSource()
     
     map_executor = MapExecutor(mapper, data_spill_saver, data_sorce, threshold=5_000)
@@ -35,40 +45,59 @@ def process_task(file_address: str):
 
     shuffler = WordCountShuffler(num_parts=4, flush_threshold=2_000)
     shuffle_executor = ShuffleExecutor(shuffler, 
-                                       source=jsonDataSource(),
-                                        sink=jsonDataSink(r"C:\ovr_pr\vss\services\worker\shuffle_files", mode="jsonl"))
-    shuffle_executor.process(r"C:\ovr_pr\vss\services\worker\spill_files")
+                                    source=jsonDataSource(),
+                                        sink=jsonDataSink(SHUFFLE_FILES_DIR, mode="jsonl"))
+    shuffle_executor.process(SPILL_FILES_DIR)
     print("Shuffling phase completed.")
+
+
+def process_reduce_task(file_address: str, part_num: int):
+        # reducing phase
+        reducer = WordCountReducer()
+        reduce_executor = ReduceExcecutor(reducer,
+                                        sink=jsonDataSink(REDUCE_OUTPUT_DIR, mode="jsonl"),
+                                        source=jsonDataSource())
+        
+        reduce_executor.process(part_dir=SHUFFLE_FILES_DIR, part_num=part_num)
+
+        print("Reducing phase completed.")
     
 
+def callback(ch, method, properties, body):
+    # if message is not valid JSON — ack and skip
+    try:
+        task = json.loads(body)
+    except Exception:
+        print(f"[{WORKER_ID}] invalid message, ack and skip")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        return
+
+    print(f"[{WORKER_ID}] picked {task.get('task_id')}")
+
+    try:
+        if task.get('type') == 'map':
+            process_map_task(task.get('address'))
+            ch.basic_ack(delivery_tag=method.delivery_tag) 
+            print(f"[{WORKER_ID}] completed {task.get('task_id')} type=map")
+        elif task.get('type') == 'reduce':
+            process_reduce_task(task.get('address'), task.get('part_id'))
+            ch.basic_ack(delivery_tag=method.delivery_tag) 
+            print(f"[{WORKER_ID}] completed {task.get('task_id')} type=reduce part_id={task.get('part_id')}")
+
+    except Exception as e:
+        print(f"[{WORKER_ID}] error processing {task.get('task_id')}: {e}")
+        # В простом варианте: nack и requeue=True (вернётся в очередь)
+        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+
+
 def main():
-    credentials = pika.PlainCredentials('admin', 'password')
-    params = pika.ConnectionParameters(host='localhost', port=5672, virtual_host='/', credentials=credentials)
+    credentials = pika.PlainCredentials(RABBIT_LOGIN, RABBIT_PASS)
+    params = pika.ConnectionParameters(host=RABBIT_HOST, port=RABBIT_PORT, virtual_host='/', credentials=credentials)
     conn = pika.BlockingConnection(params)
     ch = conn.channel()
 
     ch.queue_declare(queue=QUEUE_NAME, durable=True)
     ch.basic_qos(prefetch_count=1)
-
-    def callback(ch, method, properties, body):
-        # if message is not valid JSON — ack and skip
-        try:
-            task = json.loads(body)
-        except Exception:
-            print(f"[{WORKER_ID}] invalid message, ack and skip")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-            return
-
-        print(f"[{WORKER_ID}] picked {task.get('task_id')}")
-
-        try:
-            process_task(task.get('address'))
-            ch.basic_ack(delivery_tag=method.delivery_tag) 
-            print(f"[{WORKER_ID}] completed {task.get('task_id')}")
-        except Exception as e:
-            print(f"[{WORKER_ID}] error processing {task.get('task_id')}: {e}")
-            # В простом варианте: nack и requeue=True (вернётся в очередь)
-            ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
 
     ch.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=False)
 
