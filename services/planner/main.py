ea6f5fc2f4b7ea9417116feb0835a40cf0676a6c
Stepan <klimenkostepan30@gmail.com>
2025-11-30 01:40:29 +0300

    Added split_and_upload_txt function for uploading files to minio

--- services/planner/main.py
+++ services/planner/main.py
@@ -2,6 +2,10 @@ import pika
 import json
 import uuid
 import time
+import os
+
+from storage_client import upload_file
+
 
 QUEUE_NAME = 'tasks'
 
@@ -12,9 +16,39 @@ RABBIT_PORT = 5672
 
 TASK_ID = str(uuid.uuid4())
 
-# undone: implement file splitting
-def split_txt_file(input_file: str, lines_per_file: int = 1_000_000) -> list:
-    ...
+
+def split_and_upload_txt(input_file: str, lines_per_file: int = 1_000_000, bucket="mapreduce-data", prefix="chunks/"):
+    '''Splits a large txt file into smaller parts and uploads them to MinIO.'''
+
+    with open(input_file, 'r', encoding='utf-8') as f:
+        file_count = 0
+        lines = []
+        for line in f:
+            lines.append(line)
+            if len(lines) >= lines_per_file:
+                part_file = f"{input_file}_part{file_count}.txt"
+                with open(part_file, 'w', encoding='utf-8') as pf:
+                    pf.writelines(lines)
+                key = f"{prefix}{os.path.basename(part_file)}"
+                
+                # Upload part file to MinIO
+                upload_file(part_file, bucket, key)
+
+                os.remove(part_file)
+                file_count += 1
+                lines = []
+        if lines:
+            part_file = f"{input_file}_part{file_count}.txt"
+            with open(part_file, 'w', encoding='utf-8') as pf:
+                pf.writelines(lines)
+            key = f"{prefix}{os.path.basename(part_file)}"
+
+            upload_file(part_file, bucket, key)
+
+            os.remove(part_file)
+
+
+
 
 
 def send_map_task(ch, task_type: str, address: str):
@@ -65,5 +99,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    split_and_upload_txt(r"C:\ovr_pr\large_test_words.txt", lines_per_file=1_000_000, bucket="mapreduce", prefix="chunks/")
 
